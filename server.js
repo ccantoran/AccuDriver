d1fc62e1df416ca3aff4672f108e03b1329fde89
ccantoran <cathycantoran@live.com>
2022-08-31 22:50:59 -0500

    MVC

--- server.js
+++ server.js
@@ -1,26 +1,47 @@
-const express = require('express');
-const app = express();
-const dotenv = require('dotenv')
-dotenv.config()
-const MongoClient = require('mongodb').MongoClient
-const connectionString = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.4ccuni0.mongodb.net/?retryWrites=true&w=majority`
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const passport = require('passport')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
+const flash = require('express-flash')
+const logger = require('morgan')
+const connectDB = require('./config/database')
+const mainRoutes = require('./routes/main')
+const todoRoutes = require('./routes/todos')
 
-MongoClient.connect(connectionString, { useUnifiedTopology: true })
-  .then(client => {
-    console.log('Connected to Database')
-    const db = client.db('drivers')
-  })
-  .catch(error => console.error(error))
+require('dotenv').config({path: './config/.env'})
 
+// Passport config
+require('./config/passport')(passport)
+
+connectDB()
+
+app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+app.use(logger('dev'))
 
-app.get('/',(req, res) => {
-    res.sendFile(__dirname + '/index.html')
-})
+// Sessions
+app.use(
+    session({
+      secret: 'keyboard cat',
+      resave: false,
+      saveUninitialized: false,
+      store: new MongoStore({ mongooseConnection: mongoose.connection }),
+    })
+  )
+  
+// Passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
 
-app.listen(3000, function(){
-  console.log('listening on 3000')
-})
-//change again
+app.use(flash())
+  
+app.use('/', mainRoutes)
+app.use('/todos', todoRoutes)
+ 
+app.listen(process.env.PORT, ()=>{
+    console.log('Server is running, you better catch it!')
+})    
