f17c134bbc7ad13edd517e0622bb054518de845a
ccantoran <cathycantoran@live.com>
2022-10-19 22:50:43 -0500

    start and stop time obj established

--- server.js
+++ server.js
@@ -59,5 +59,5 @@ app.use("/driver", driverRoutes);
 
 //Server Running
 app.listen(process.env.PORT, () => {
-  console.log("Server is running, you better catch it!");
+  console.log("Server is running");
 });
