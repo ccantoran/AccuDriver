5a9d698192ff0193967dda702680e7ad56bf97f4
ccantoran <cathycantoran@live.com>
2022-11-10 12:00:40 -0600

    change out .js

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,6 @@
 const passport = require("passport");
 const validator = require("validator");
-const User = require("../models/User.js");
+const User = require("../models/User");
 
 exports.getLogin = (req, res) => {
   if (req.user) {
