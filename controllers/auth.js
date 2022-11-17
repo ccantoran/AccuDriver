4cf43af9e7635eadaf0ea29504a51582cf8fa5bb
ccantoran <cathycantoran@live.com>
2022-11-17 13:12:36 -0600

    path change

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,6 @@
 const passport = require("passport");
 const validator = require("validator");
-const User = require("../models/User");
+const User = require("User");
 // import {User} from "../models/User"
 
 exports.getLogin = (req, res) => {
