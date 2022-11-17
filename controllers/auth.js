05a727def0333b8594c8a8204729f36c1979dcfe
ccantoran <cathycantoran@live.com>
2022-11-17 14:07:01 -0600

    change

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,6 @@
 const passport = require("passport");
 const validator = require("validator");
-const User = require("User.js");
+const User = require("/User.js");
 // import {User} from "../models/User"
 
 exports.getLogin = (req, res) => {
